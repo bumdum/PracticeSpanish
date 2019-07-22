f91c312e42d438e003f33d1eb528c4d4902cd917
Boyle <jboyle@cdsw.com>
2019-07-22 14:57:56 -0700

    update

--- src/app/shared/layouts/custom-material/custom-material.module.ts
+++ src/app/shared/layouts/custom-material/custom-material.module.ts
@@ -1,23 +1,21 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import {
-  MatButtonModule,
-  MatMenuModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule,
-  MatCheckboxModule, 
-  MatSidenavModule,
-  MatListModule,
-  MatFormFieldModule,
-  MatProgressSpinnerModule,
-  MatInputModule,
-  MatChipsModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatTabsModule,
-  MatTableModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
